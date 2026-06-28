1550608bc79e7933a36ab92f8cdbf644c848d303
Bruno Medeiros <brunomedeiros0121@gmail.com>
2026-06-28 01:00:12 -0300

    debug: add launch.log to diagnose Windows startup failure

--- plugin/launch.bat
+++ plugin/launch.bat
@@ -2,18 +2,34 @@
 setlocal enabledelayedexpansion
 
 set "SCRIPT_DIR=%~dp0"
+set "LOGDIR=%SCRIPT_DIR%log"
 
-for %%N in (
-    "%PROGRAMFILES%\HotSpot\StreamDock\Helpers\node20.exe"
-    "%PROGRAMFILES(X86)%\HotSpot\StreamDock\Helpers\node20.exe"
-    "%LOCALAPPDATA%\Programs\HotSpot\StreamDock\Helpers\node20.exe"
-    "%PROGRAMFILES%\fifine Control Deck\Helpers\node20.exe"
-    "%LOCALAPPDATA%\Programs\fifine Control Deck\Helpers\node20.exe"
-) do (
-    if exist %%N (
-        %%N "%SCRIPT_DIR%build\index.js" %*
-        exit /b
-    )
+if not exist "%LOGDIR%" mkdir "%LOGDIR%" 2>nul
+echo [%DATE% %TIME%] launch.bat started > "%LOGDIR%\launch.log"
+echo SCRIPT_DIR: %SCRIPT_DIR% >> "%LOGDIR%\launch.log"
+
+set "NODE_EXE="
+
+if exist "%PROGRAMFILES%\HotSpot\StreamDock\Helpers\node20.exe" (
+    set "NODE_EXE=%PROGRAMFILES%\HotSpot\StreamDock\Helpers\node20.exe"
+) else if exist "%LOCALAPPDATA%\Programs\HotSpot\StreamDock\Helpers\node20.exe" (
+    set "NODE_EXE=%LOCALAPPDATA%\Programs\HotSpot\StreamDock\Helpers\node20.exe"
+) else if exist "%PROGRAMFILES(X86)%\HotSpot\StreamDock\Helpers\node20.exe" (
+    set "NODE_EXE=%PROGRAMFILES(X86)%\HotSpot\StreamDock\Helpers\node20.exe"
+) else if exist "%PROGRAMFILES%\fifine Control Deck\Helpers\node20.exe" (
+    set "NODE_EXE=%PROGRAMFILES%\fifine Control Deck\Helpers\node20.exe"
+) else if exist "%LOCALAPPDATA%\Programs\fifine Control Deck\Helpers\node20.exe" (
+    set "NODE_EXE=%LOCALAPPDATA%\Programs\fifine Control Deck\Helpers\node20.exe"
 )
 
-node "%SCRIPT_DIR%build\index.js" %*
+if "%NODE_EXE%"=="" (
+    echo No bundled node found, trying system node >> "%LOGDIR%\launch.log"
+    where node >> "%LOGDIR%\launch.log" 2>&1
+    set "NODE_EXE=node"
+) else (
+    echo Found node: %NODE_EXE% >> "%LOGDIR%\launch.log"
+)
+
+echo Running node with args: %* >> "%LOGDIR%\launch.log"
+"%NODE_EXE%" "%SCRIPT_DIR%build\index.js" %*
+echo Exit code: %ERRORLEVEL% >> "%LOGDIR%\launch.log"
